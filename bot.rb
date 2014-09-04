4fb52ae21da594afb6afc4cc0eca0d4eac119afd
Joe Truba <joe@bannable.us>
2014-09-04 14:59:12 -0700

    We need to choose the column we're searching for if we want data

--- bot.rb
+++ bot.rb
@@ -12,18 +12,21 @@ if (DEVELOPMENT)
 	require './tests/dm_tests'
 end
 
-@game_mode = 'unknown'
+$game_mode = 'unknown'
 
 def update_mode(input)
 	case input
-	when 'more'
-		@game_mode = 'mm'
+	when 'matchmaking'
+		$game_mode = 'mm'
 	when 'characters'
-		@game_mode = 'to'
+		$game_mode = 'to'
 	when 'exhibition'
-		@game_mode = 'ex'
+		$game_mode = 'ex'
 	else
-		@game_mode = 'unknown'
+		$game_mode = 'unknown'
+	end
+	if (DEVELOPMENT)
+	       	puts "Game mode updated to: " << $game_mode.upcase
 	end
 end
 
@@ -46,50 +49,53 @@ scraper = Cinch::Bot.new do
 		puts 'Bot is now ready for action.'
 	end
 
-
 	on :message, PATTERN_NEW do |m|
-		return unless m.user == "waifu4u"
-		if (PATTERN_NEW_SPLIT =~ m.message)
+		if (m.user == "waifu4u" && PATTERN_NEW_SPLIT =~ m.message)
+			if (DEVELOPMENT) 
+				puts "Game mode is currently: " << $game_mode.upcase
+			end
 			update_mode($4)
-			if @game_mode == 'ex'
+			if ($game_mode == 'ex')
 				# Do not create entries from Exhibition fights
 				puts "Red: " << $1
 				puts "Blue: " << $2
+			elsif ($game_mode == 'unknown')
+				puts "How did we get here?"
+			       	puts m
+				exit
 			else
-				red = Fighter::first_or_create($1)
-				blue = Fighter::first_or_create($2)
+				red = Fighter::first_or_create(:name => $1)
+				blue = Fighter::first_or_create(:name => $2)
 				puts "Red: " << red.name
 				puts "Blue: " << blue.name
 			end
-			puts "Mode: " << @game_mode.upcase << " (Tier: #{$3})"
+			puts "Mode: " << $game_mode.upcase << " (Tier: #{$3})"
 		end
 	end
 
 	on :message, PATTERN_START do |m|
-		return unless m.user == "waifu4u"
-		if (PATTERN_START_SPLIT =~ m.message)
-			if (@game_mode == nil)
-				# Our state is unknown. Do not record match.
-				return
-			elsif (@game_mode == 'ex')
+		if (m.user == "waifu4u" && PATTERN_START_SPLIT =~ m.message)
+			if (DEVELOPMENT)
+			       	puts "Game mode is currently: " << $game_mode.upcase
+			end
+			if ($game_mode == 'ex')
 				# Do not create entries from Exhibition fights
 				puts "Red: " << $1 << " (#{$2})"
 				puts "Blue: " << $3 << " (#{$4})"
-			else
-				red = Fighter::first_or_create($1)
-				blue = Fighter::first_or_create($3)
-				puts "Red: " << red.name << " (#{$2})"
-				puts "Blue: " << blue.name << " (#{$4})"
-				puts "Current mode is: " << @game_mode.upcase
+			elsif ($game_mode != 'unknown')
+				red = Fighter::first_or_create(:name => $1)
+				blue = Fighter::first_or_create(:name => $3)
+				puts "Red: " << red.name << "[#{$1}] (#{$2})"
+				puts "Blue: " << blue.name << "[#{$3}] (#{$4})"
+				puts "Current mode is: " << $game_mode.upcase
 			end
 		end
 	end	
 
 	on :message, PATTERN_END do |m|
-		return unless m.user == "waifu4u"
-		if (PATTERN_END =~ m.message)
+		if (m.user == "waifu4u" && PATTERN_END =~ m.message)
 			puts "Winner #{$1} (#{$2})"
-			puts "Current mode " << @game_mode.upcase << " changes in #{$3} matches"
+			puts "Current mode " << $game_mode.upcase << " changes in #{$3} matches"
 		end
 	end
 
