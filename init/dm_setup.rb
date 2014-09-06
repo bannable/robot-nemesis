20d40b9f2115f6976e4ca6c44a8ce8dd085b329d
Joe Truba <joe@bannable.us>
2014-09-05 17:27:13 -0700

    Initialize databases other than sqlite correctly

--- init/dm_setup.rb
+++ init/dm_setup.rb
@@ -3,6 +3,8 @@ if (DEVELOPMENT)
 	DataMapper::Logger.new($stdout, :debug)
 	if (CONFIG['dev_db'])
 		DataMapper.setup(:default, 'sqlite:development.db')
+	else
+		DataMapper.setup(:default, CONFIG['db_driver'])
 	end
 else
 	DataMapper.setup(:default, CONFIG['db_driver'])
