dd8a643172048aa9d8bed7e0317350944f9a89f1
Joe Truba <joe@bannable.us>
2014-09-06 20:48:28 -0700

    Handle erroneous lines in the import tool better

--- import.rb
+++ import.rb
@@ -1,21 +1,25 @@
 require './models/setup.rb'
 
 def parse(line)
-	/^(?<mred>.*?) - \$(?<red_bet>[\d]+), (?<mblue>.*?) - \$(?<blue_bet>[\d]++)\s+(?<winner>.*?)\s+\d{1,2}:/ =~ line
-	puts "#{mred},#{red_bet},#{mblue},#{blue_bet},#{winner}"
-	blue = Fighter::first_or_create(:name => mblue)
-	red = Fighter::first_or_create(:name => mred)
-	r_blue = Rating.new(
-		:old_rating => blue.rating,
-		:opp_rating => red.rating,
-		:k_factor => blue.k_factor
-	)
-	r_red = Rating.new(
-		:old_rating => red.rating,
-		:opp_rating => blue.rating,
-		:k_factor => red.k_factor
-	)
-	Match::play(red, blue, red_bet, blue_bet, r_red, r_blue, nil, winner)
+	if /^(?<mred>.*?) - \$(?<red_bet>[\d]+), (?<mblue>.*?) - \$(?<blue_bet>[\d]++)\s+(?<winner>.*?)\s+\d{1,2}:/ =~ line
+		puts "#{mred},#{red_bet},#{mblue},#{blue_bet},#{winner}"
+		blue = Fighter::first_or_create(:name => mblue)
+		red = Fighter::first_or_create(:name => mred)
+		r_blue = Rating.new(
+			:old_rating => blue.rating,
+			:opp_rating => red.rating,
+			:k_factor => blue.k_factor
+		)
+		r_red = Rating.new(
+			:old_rating => red.rating,
+			:opp_rating => blue.rating,
+			:k_factor => red.k_factor
+		)
+		if (winner != red.name && winner != blue.name)
+			winner = nil
+		end
+		Match::play(red, blue, red_bet, blue_bet, r_red, r_blue, nil, winner)
+	end
 end
 
 it = 0
