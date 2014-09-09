0c2179f7573d6507ae2ce0269b652aa49d33f754
Joe Truba <joe@bannable.us>
2014-09-08 17:15:00 -0700

    Introduce the framework for our stats tracking later on

---
+++ helpers/stats.rb
@@ -0,0 +1,3 @@
+def close_guess(left,right)
+	(left - right).abs <= GUESS_RANGE
+end
--- models/setup.rb
+++ models/setup.rb
@@ -10,6 +10,7 @@ require './models/rating'
 require './models/fighter'
 require './models/match'
 require './models/fighter_match'
+require './helpers/stats'
 
 # Fighter (ID) <- FighterMatch(Fighter ID, Match ID, Color) <- Match(ID, Victor, timestamp)
 DataMapper.finalize.auto_upgrade!
@@ -19,3 +20,4 @@ PATTERN_NEW_SPLIT	= /^Bets are OPEN for (.*?) vs (.*?)! \(([XSABP]|NEW) Tier\)(?
 PATTERN_START		= /^Bets are locked/
 PATTERN_START_SPLIT	= /^Bets are locked\. (.*?)(?: \([\d-]+\) |)- \$((?:[\d,]+)), (.*?)(?: \([\d-]+\) |)- \$((?:[\d,]+))/
 PATTERN_END		= /^((?:.*)) wins! (?:.*) Team (Red|Blue)\. ((?:[\d]+)) (more|characters|exhibition)/
+GUESS_RANGE		= 0.05
